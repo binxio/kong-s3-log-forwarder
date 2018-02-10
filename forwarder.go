b1d89571df3aaafdaaed031e434541ac4134501a
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2018-02-10 20:11:35 +0100

    determine whether to use https or http based on the -cert-file and -key-file flags

--- forwarder.go
+++ forwarder.go
@@ -23,26 +23,41 @@ func main() {
 }
 
 func NewS3LogForwarder() *S3LogForwarder {
+	var port int
+	var address string
 	result := new(S3LogForwarder)
 	result.channel = make(chan []byte, 2048)
 
 	flag.BoolVar(&result.verbose, "verbose", false, "log level")
 
-	flag.BoolVar(&result.https, "https", false, "listen using")
-	flag.BoolVar(&result.https, "server-certificate", false, "listen using")
-	flag.IntVar(&result.listenPort, "listen-port", 4443, "of the log forwarder")
-	flag.StringVar(&result.listenAddress, "listen-address", "0.0.0.0", "of the log forwarder")
+	flag.StringVar(&result.certFile, "cert-file", "", "identifying the server")
+	flag.StringVar(&result.keyFile, "key-file", "", "of the server")
+	flag.IntVar(&port, "listen-port", 4443, "of the log forwarder")
+	flag.StringVar(&address, "listen-address", "0.0.0.0", "of the log forwarder")
 
 	flag.StringVar(&result.bucketName, "bucket-name", "", "to write to")
 	flag.StringVar(&result.bucketRegion, "region", "", "of the s3 bucket")
-	flag.StringVar(&result.keyPrefix, "key-prefix", "KongLogs", "for the s3 bucket key")
+	flag.StringVar(&result.keyPrefix, "key-prefix", "AppLogs", "for the s3 bucket key")
 
 	flag.DurationVar(&result.flushPeriod, "period", time.Duration(time.Second*30), "minimum size between flushes to s3")
-	flag.IntVar(&result.cacheSize, "cache-size", 32 * 1024, "minimum size of the individual s3 objects")
+	flag.IntVar(&result.cacheSize, "cache-size", 32*1024, "maximum size of the individual s3 objects")
 
 	flag.Parse()
+	result.keyPrefix = strings.TrimRight(result.keyPrefix, "/")
 	if result.bucketName == "" {
 		log.Fatal("no bucket name specified.")
+	}
+
+	session, err := session.NewSession()
+	if err != nil {
+		log.Fatal("sessionNewSession", err)
+	}
+
+	if result.bucketRegion == "" {
+		result.bucketRegion = *session.Config.Region;
+		if result.bucketRegion == "" {
+			log.Fatal("no bucket region specified.")
+		}
 	}
 
 	if result.cacheSize <= 0 {
@@ -53,36 +68,51 @@ func NewS3LogForwarder() *S3LogForwarder {
 		log.Fatal("flush period cannot be less than 0")
 	}
 
-	result.keyPrefix = strings.TrimRight(result.keyPrefix, "/")
+	if result.keyFile != "" || result.certFile != "" {
+		result.protocol = "https"
+	} else {
+		result.protocol = "http"
+	}
 
-	session, err := session.NewSession()
-	if result.bucketRegion == "" {
-		fmt.Print(*session.Config.Region)
-		result.bucketRegion = *session.Config.Region;
-		if result.bucketRegion == "" {
-			log.Fatal("no bucket region specified.")
+	if result.protocol == "https" {
+		if result.certFile == "" {
+			log.Fatal("missing --cert-file")
+		}
+		if result.keyFile == "" {
+			log.Fatal("missing --key-file")
+
+		}
+		if s, err := os.Stat(result.certFile); err != nil || s.IsDir() {
+			log.Fatalf("%s is not a file", result.certFile)
+		}
+
+		if s, err := os.Stat(result.keyFile); err != nil || s.IsDir() {
+			log.Fatalf("%s is not a file", result.keyFile)
 		}
 	}
 
-	if err != nil {
-		log.Fatal("sessionNewSession", err)
-	}
+	result.listenAddr = fmt.Sprintf("%s:%d", address, port)
 	result.s3 = s3.New(session, &aws.Config{Region: aws.String(result.bucketRegion)})
 
 	return result
 }
 
 func (f *S3LogForwarder) serve() {
+	var err error
 	go f.bufferedPut()
 
 	if f.verbose {
+		log.Printf("listening on %s://%s\n", f.protocol, f.listenAddr)
 		log.Printf("forwarding to s3 bucket %s in %s\n", f.bucketName, f.bucketRegion)
 		log.Printf("flush period: %s, cache size %d\n", f.flushPeriod.String(), f.cacheSize)
 	}
 
 	http.HandleFunc("/", f.KongLogForwarder)
-	err := http.ListenAndServeTLS(fmt.Sprintf("%s:%d", f.listenAddress, f.listenPort), "server.crt", "server.key", nil)
-
+	if f.protocol == "https" {
+		err = http.ListenAndServeTLS(f.listenAddr, f.certFile, f.keyFile, nil)
+	} else {
+		err = http.ListenAndServe(f.listenAddr, nil)
+	}
 	close(f.channel)
 
 	if err != nil {
@@ -108,6 +138,8 @@ func (f *S3LogForwarder) KongLogForwarder(w http.ResponseWriter, req *http.Reque
 func (f *S3LogForwarder) bufferedPut() {
 	var buffer bytes.Buffer
 	timer := time.NewTimer(f.flushPeriod)
+
+	defer timer.Stop()
 	for {
 		select {
 		case body, more := <-f.channel:
@@ -122,23 +154,21 @@ func (f *S3LogForwarder) bufferedPut() {
 				return
 			}
 		case <-timer.C:
-			f.putObject(&buffer)
-			timer.Reset(f.flushPeriod)
+			if buffer.Len() > 0 {
+				f.putObject(&buffer)
+				timer.Reset(f.flushPeriod)
+			}
 		}
 	}
 }
 
 func (f *S3LogForwarder) putObject(buffer *bytes.Buffer) {
+	key := f.newS3Key()
+	if f.verbose {
+		fmt.Printf("writing buffer of %d bytes to %s\n", buffer.Len(), key)
+	}
+
 	if buffer.Len() > 0 {
-		now := time.Now()
-		uuid := newUUID()
-		hostName, _ := os.Hostname()
-		key := fmt.Sprintf("%s/%04d/%02d/%02d/%04d%02d%02dT%02d%02d%02d.%06dZ-%s-%x.log",
-			f.keyPrefix, now.Year(), now.Month(), now.Day(), now.Year(), now.Month(), now.Day(),
-			now.Hour(), now.Minute(), now.Second(), now.Nanosecond(), hostName, uuid)
-		if f.verbose {
-			fmt.Printf("writing buffer of %d bytes to %s\n", buffer.Len(), key)
-		}
 		request := s3.PutObjectInput{
 			Bucket:          &f.bucketName,
 			Body:            bytes.NewReader(buffer.Bytes()),
@@ -154,6 +184,15 @@ func (f *S3LogForwarder) putObject(buffer *bytes.Buffer) {
 	}
 }
 
+func (f *S3LogForwarder) newS3Key() string {
+	now := time.Now()
+	uuid := newUUID()
+	hostName, _ := os.Hostname()
+	return fmt.Sprintf("%s/%04d/%02d/%02d/%04d%02d%02dT%02d%02d%02d.%06dZ-%s-%x.log",
+		f.keyPrefix, now.Year(), now.Month(), now.Day(), now.Year(), now.Month(), now.Day(),
+		now.Hour(), now.Minute(), now.Second(), now.Nanosecond(), hostName, uuid)
+}
+
 func newUUID() []byte {
 	uuid := make([]byte, 16)
 	n, err := io.ReadFull(rand.Reader, uuid)
@@ -166,11 +205,13 @@ func newUUID() []byte {
 type S3LogForwarder struct {
 	channel chan []byte
 
-	verbose       bool
+	verbose bool
 
-	https         bool
-	listenPort    int
-	listenAddress string
+	protocol string
+	certFile string
+	keyFile  string
+
+	listenAddr string
 
 	keyPrefix    string
 	bucketRegion string
